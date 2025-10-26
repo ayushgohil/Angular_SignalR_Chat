778ca9ba0890a4b5dd2b01fca968af7f690ef70d
Ayush Gohil <ayush.gohil12@gmail.com>
2025-10-26 20:42:15 +0530

    Added main-module.component

---
+++ Chat_APP/src/app/main-module/main-module.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MenuContentComponent } from '../menu-content/menu-content.component';
+
+@Component({
+  selector: 'app-main-module',
+  imports: [SidebarComponent, MenuContentComponent],
+  templateUrl: './main-module.component.html',
+  styleUrl: './main-module.component.css'
+})
+export class MainModuleComponent {
+
+}
