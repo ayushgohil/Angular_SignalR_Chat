9d6f0f5513f34f0292bd9e1ca93b716b8070e331
Ayush Gohil <ayush.gohil12@gmail.com>
2025-12-10 23:25:17 +0530

    Added ChatUsersDTO

---
+++ Chat_API/Chat_API/Models/ChatUsersDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat_API.Models
+{
+    public class ChatUsersDTO
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string AvatarURL { get; set; }
+
+    }
+}
